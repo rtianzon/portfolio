2bed99af0a132cee0bafc105c3b33bd29c68d85f
rtianzon <tianzonrupert@gmail.com>
2023-07-06 04:11:30 +0800

    u

--- routes/scheduling2/home.js
+++ routes/scheduling2/home.js
@@ -5,4 +5,8 @@ router.get('/', async (req, res) => {
     res.render("scheduling2/html/home")
 })
 
+router.get('/daterange', async (req, res) => {
+    res.render("scheduling2/html/daterange")
+})
+
 module.exports = router
